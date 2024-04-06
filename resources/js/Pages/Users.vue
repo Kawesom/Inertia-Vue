557b290d3d9eb12123ece5719220ffc8c608479c
Kawesom <Kamaladeyinka@yahoo.com>
2024-04-07 09:40:45 +1000

    Ep 8 - Preserve Scroll

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -4,12 +4,23 @@
     </h1>
 
     <Nav/>
+
+    <div style="margin-top: 400px">
+        <p>The Current time is {{ time }}.</p>
+
+        <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
+    </div>
 </template>
 
 <script>
 import Nav from "../Shared/Nav.vue";
+import { Link } from "@inertiajs/vue3";
 
 export default {
-    components: { Nav },
+    components: { Nav, Link },
+
+    props: {
+        time: String
+    }
 }
 </script>
